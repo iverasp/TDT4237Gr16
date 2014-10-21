0532a54f53c21fd5133f09391868829f57b4b2cc
Iver Egge <iverasp@gmail.com>
2014-10-21 12:00:38 +0200

    typo related to issue 3.4

--- src/webapp/controllers/UserController.php
+++ src/webapp/controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
         $request = $this->app->request;
         $username = $request->post('user');
         $pass = $request->post('pass');
-        if (strlen($pass) <= 8) {
+        if (strlen($pass) < 8) {
             $this->app->flash('info', 'Password must be longer than 8 characters');
             $this->app->redirect('/user/new');
         }
