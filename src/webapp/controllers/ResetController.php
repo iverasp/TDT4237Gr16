5a780caa87944e37f6b144f85f292af2ce68d52b
Iver Egge <iverasp@gmail.com>
2014-10-21 17:22:56 +0200

    coooode

--- src/webapp/controllers/ResetController.php
+++ src/webapp/controllers/ResetController.php
@@ -3,6 +3,7 @@
 namespace tdt4237\webapp\controllers;
 
 use tdt4237\webapp\Auth;
+use tdt4237\webapp\models\User;
 
 class ResetController extends Controller
 {
@@ -14,6 +15,18 @@ class ResetController extends Controller
     function index()
     {
         $this->render('reset.twig');
+    }
+
+    function reset() {
+        $request = $this->app->request;
+        $username = Controller::process_url_params($request->post('email'));
+        if (User::findByUser($username)) {
+        
+        } else {
+            $this->app->flash('info', 'User not found in database');
+            $this->app->redirect('/reset');
+            return;
+        }
     }
 
 }
